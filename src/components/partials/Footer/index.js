fd11e8e3f5b6b9f4f890c68779acbec2e4ee2e3d
Douglas Davi <douglassromano@gmail.com>
2021-04-27 13:16:37 -0300

    Add páginação e rodapé

--- src/components/partials/Footer/index.js
+++ src/components/partials/Footer/index.js
@@ -4,7 +4,8 @@ import {FooterArea} from './styled'
 const Footer = () =>{
     return(
         <FooterArea>
-            ...
+            Todos os direitor reservados<br />
+            OLX Clone - Douglas Romano
         </FooterArea>
     ) 
 
--- src/pages/Ads/index.js
+++ src/pages/Ads/index.js
@@ -22,46 +22,70 @@ const Page = () =>{
     const [cat, setCat] = useState( query.get('cat') != null ? query.get('cat') : '' );
     const [state, setState] = useState( query.get('state') != null ? query.get('state') : '' );
 
+    const [adsTotal, setAdsTotal] = useState(0)
     const [stateList, setStateList] = useState([]);
     const [categories, setCategories] = useState([]);
     const [adList, setAdList] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
 
-    const [resultOpacity, setResultOpacity] = useState(0.3)
+    const [resultOpacity, setResultOpacity] = useState(0.3);
+    const [loading, setLoading] = useState(true);
 
     const getAdsList = async () =>{
+        setLoading(true);
+        let offset = (currentPage -1) * 9;
+        console.log(offset)
         const json = await api.getAds({
             sort: 'desc',
-            limit:9,
+            limit: 9,
             q,
             cat,
-            state
+            state,
+            offset
         })
-        setAdList(json.ads)
-        setResultOpacity(1)
+        setAdList(json.ads);
+        setAdsTotal(json.total);
+        setResultOpacity(1);
+        setLoading(false);
     }
+
+    useEffect(()=>{
+        if(adList.length > 0){
+            setPageCount(Math.ceil( adsTotal / adList.length ))
+        }else{
+            setPageCount(0);
+        }
+    },[adsTotal]);
+
+    useEffect(()=>{
+        setResultOpacity(0.3);
+        getAdsList();
+    },[currentPage]);
 
     useEffect(()=>{
         let queryString = []
         if(q){
-            queryString.push(`q=${q}`)
+            queryString.push(`q=${q}`);
         }
         if(cat){
-            queryString.push(`cat=${cat}`)
+            queryString.push(`cat=${cat}`);
         }
         if(state){
-            queryString.push(`state=${state}`)
+            queryString.push(`state=${state}`);
         }
 
         history.replace({
             search:`?${queryString.join('&')}`
-        })
+        });
 
         if(timer){
-            clearTimeout(timer)
+            clearTimeout(timer);
         }
 
-        timer = setTimeout(getAdsList, 2000)
-        setResultOpacity(0.3)
+        timer = setTimeout(getAdsList, 2000);
+        setResultOpacity(0.3);
+        setCurrentPage(1);
     }, [q,cat,state])
 
     useEffect(()=>{
@@ -80,7 +104,10 @@ const Page = () =>{
         getCategories();
     }, []);
 
-
+    let pagination = [];
+    for(let i=1;i<=pageCount;i++){
+        pagination.push(i)
+    }
 
     return(
        <PageContaniner>
@@ -120,11 +147,24 @@ const Page = () =>{
                 </div>
                 <div className="rightSide">
                     <h2>Resultados</h2>
+                    {loading && adList.length === 0 &&
+                        <div className="listWarning">Carregando...</div>
+                    }
+                    {!loading && adList.length === 0 &&
+                        <div className="listWarning">Não Encontramos Resultados...</div>
+                    }
                     <div className="list" style={{opacity: resultOpacity}}>
                         {adList.map((i,k) =>
                             <AdItem key={k} data={i} />
                         )}
                     </div>
+                    
+                    <div className="pagination">
+                        {pagination.map((i,k)=>
+                            <div onClick={()=>setCurrentPage(i)} className={i===currentPage?'pagItem active':'pagItem'}>{i}</div>
+                        )}
+                    </div>
+
                 </div>
 
            </PageArea>
